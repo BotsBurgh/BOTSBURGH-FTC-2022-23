5c53d56615c9031a381b4a650062c03ea0c93b1a
BD103 <59022059+BD103@users.noreply.github.com>
2022-10-23 18:11:42 -0400

    feat: add empty teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EmptyTeleOp.kt
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.api.arch.Config
+import org.firstinspires.ftc.teamcode.api.arch.Robot
+
+/**
+ * This is an empty teleop for debugging purposes.
+ * If a programmer has a quick component to test, use this instead of [TeleOpMain].
+ *
+ * By default, this is disabled. To fix this, remove the `@Disabled` annotation below.
+ * To register components, add them to the [EmptyRobot] class.
+ *
+ * > **Note:** Please make sure not to commit your test changes in this file.
+ */
+@TeleOp(name = "Empty")
+@Disabled
+class EmptyTeleOp: LinearOpMode() {
+    override fun runOpMode() {
+        EmptyRobot(this, Config()).run()
+    }
+}
+
+/**
+ * This is an empty [Robot] class. This manages all components being registered.
+ * The actual [Robot] class used in production is [TriRobot][org.firstinspires.ftc.teamcode.api.TriRobot].
+ */
+class EmptyRobot(teleop: LinearOpMode, cfg: Config): Robot(teleop, cfg) {
+    override fun registerComponents() {
+        // Register any components here!
+        // this.register(MyComponent())
+    }
+}
