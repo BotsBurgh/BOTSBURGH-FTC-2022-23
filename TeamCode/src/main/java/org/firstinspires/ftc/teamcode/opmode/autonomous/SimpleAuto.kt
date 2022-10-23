948214ffe22cbfcf003fcf1a13271333947301bc
BD103 <59022059+BD103@users.noreply.github.com>
2022-10-23 18:22:21 -0400

    feat(auto): create a template autonomous
    Disabled and TODO-d for now.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/SimpleAuto.kt
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmode.autonomous
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+
+@Autonomous(name = "Simple Autonomous")
+@Disabled
+class SimpleAuto: LinearOpMode() {
+    override fun runOpMode() {
+        TODO("Not yet implemented")
+    }
+}
