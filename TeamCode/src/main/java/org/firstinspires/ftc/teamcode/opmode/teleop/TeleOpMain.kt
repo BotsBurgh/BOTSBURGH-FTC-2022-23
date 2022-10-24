6d1677c23eb4739e4e7e00329b122a0453a92186
BD103 <59022059+BD103@users.noreply.github.com>
2022-10-24 15:35:43 -0400

    feat: document teleop main

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpMain.kt
@@ -5,6 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.api.TriRobot
 import org.firstinspires.ftc.teamcode.api.arch.Config
 
+/**
+ * This is the main teleop run during competition.
+ *
+ * For most of the part, this file will not be edited.
+ * To find components being registered, take a look at the [TriRobot] class.
+ * For an empty class used just for testing singular components, look at [EmptyTeleOp].
+ */
 @TeleOp(name = "TeleOp Main")
 class TeleOpMain: LinearOpMode() {
     override fun runOpMode() {
