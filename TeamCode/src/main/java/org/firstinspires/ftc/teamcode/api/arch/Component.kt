aebbc472606dda88fc089fa8ced46bb35f2155fc
BD103 <59022059+BD103@users.noreply.github.com>
2022-11-16 22:16:19 -0500

    feat(arch): create constants for component defaults

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/arch/Component.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/arch/Component.kt
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.api.arch
 
 typealias ComponentFunction = (ctx: Context) -> Unit
 
+const val DEFAULT_ORDER: Byte = 0
+// Enums can't be constants :(
+val DEFAULT_OPMODE: OpMode = OpMode.Any
+
 /**
  * An open class that all components should extend.
  */
@@ -28,10 +32,10 @@ abstract class Component {
      * Smaller numbers are run earlier in the phase, larger numbers are run later in the phase.
      * When multiple components have the same ordering, the [runtime builder][org.firstinspires.ftc.teamcode.api.arch.RuntimeBuilder] chooses which goes first.
      */
-    open val order: Byte = 0
+    open val order: Byte = DEFAULT_ORDER
 
     /**
      * Determines what kind of opmode this component should be run on.
      */
-    open val opmode: OpMode = OpMode.Any
+    open val opmode: OpMode = DEFAULT_OPMODE
 }
