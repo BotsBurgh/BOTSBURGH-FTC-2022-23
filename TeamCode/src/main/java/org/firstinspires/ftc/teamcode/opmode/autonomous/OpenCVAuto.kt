aa06ab6dcac910bcdd684837f47c81e61f63d485
BD103 <59022059+BD103@users.noreply.github.com>
2022-12-11 17:39:08 -0500

    feat: image cropping and blurring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/OpenCVAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/OpenCVAuto.kt
@@ -2,10 +2,13 @@ package org.firstinspires.ftc.teamcode.opmode.autonomous
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
+import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
 import org.openftc.easyopencv.OpenCvCameraFactory
 import org.openftc.easyopencv.OpenCvCameraRotation
 import org.openftc.easyopencv.OpenCvInternalCamera
+import org.openftc.easyopencv.OpenCvPipeline
 
 @Autonomous(name = "OpenCV Auto")
 class OpenCVAuto: LinearOpMode() {
@@ -16,8 +19,10 @@ class OpenCVAuto: LinearOpMode() {
             cameraMonitorViewId
         )
 
+        camera.setPipeline(TestPipeline())
+
         camera.openCameraDevice()
-        camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT)
+        camera.startStreaming(320, 240, OpenCvCameraRotation.UPSIDE_DOWN)
 
         telemetry.addData("Status", "Initialized aa")
         telemetry.update()
@@ -26,12 +31,33 @@ class OpenCVAuto: LinearOpMode() {
 
         camera.stopStreaming()
         camera.closeCameraDevice()
+    }
+}
 
-        /*
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Running")
-            telemetry.update()
-        }
-         */
+class TestPipeline: OpenCvPipeline() {
+    enum class Color {
+        Red,
+        Green,
+        Blue,
+    }
+
+    var output = Color.Red
+        private set
+
+    private var input: Mat? = null
+    private var blurred = Mat()
+
+    override fun init(_firstFrame: Mat?) {
+        val firstFrame = _firstFrame!!
+
+        val left = (firstFrame.width() / 2) - 50
+        val top = (firstFrame.height() / 2) - 50
+
+        input = firstFrame.submat(left, left + 100, top, top + 100)
+    }
+
+    override fun processFrame(_input: Mat?): Mat {
+        Imgproc.blur(input, blurred, Size(100.0, 100.0))
+        return blurred
     }
 }
