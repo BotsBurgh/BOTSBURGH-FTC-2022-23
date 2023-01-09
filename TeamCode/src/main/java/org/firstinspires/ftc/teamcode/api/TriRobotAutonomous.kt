65b1d90470f7e37d7730e1ba9a05d3d6e7fc7332
BD103 <59022059+BD103@users.noreply.github.com>
2023-01-08 19:03:29 -0500

    feat: begin scan and park

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/TriRobotAutonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/TriRobotAutonomous.kt
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode.api
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.teamcode.api.plugins.Wheels
+import org.firstinspires.ftc.teamcode.api.plugins.opencv.OpenCV
 import org.firstinspires.ftc.teamcode.api.plugins.wheels
 import org.firstinspires.ftc.teamcode.api.steps.LoggerAuto
+import org.firstinspires.ftc.teamcode.api.steps.ScanAndPark
 import org.firstinspires.ftc.teamcode.arch.sequential.SequentialRobot
 import org.firstinspires.ftc.teamcode.arch.sequential.SequentialRuntimeBuilder
 
@@ -11,9 +13,8 @@ class TriRobotAutonomous(teleop: LinearOpMode): SequentialRobot(teleop) {
     override fun configure(builder: SequentialRuntimeBuilder) {
         builder
             .registerPlugin(Wheels())
+            .registerPlugin(OpenCV())
             .registerStep(LoggerAuto())
-            .registerMain {
-                it.wheels.driveEncoderDirection(0.0, 24.0)
-            }
+            .registerStep(ScanAndPark())
     }
 }
