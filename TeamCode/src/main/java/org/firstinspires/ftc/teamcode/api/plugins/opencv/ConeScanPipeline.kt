1cb8a79ebe2ae0c45289260f33d5f0d92fd29aae
BD103 <59022059+BD103@users.noreply.github.com>
2023-01-30 13:52:29 -0500

    feat: add cone scanning configuration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/plugins/opencv/ConeScanPipeline.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/plugins/opencv/ConeScanPipeline.kt
@@ -1,7 +1,29 @@
 package org.firstinspires.ftc.teamcode.api.plugins.opencv
 
+import com.acmerobotics.dashboard.config.Config
 import org.opencv.core.Mat
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
 import org.openftc.easyopencv.OpenCvPipeline
+
+@Config
+private object ConeScanConfig {
+    /**
+     * The value to multiply against a pixel's color in order to increase / decrease **contrast**.
+     *
+     * `NEW_IMG = IMAGE_MULTIPLIER * IMG + IMAGE_OFFSET`
+     */
+    @JvmField var IMAGE_MULTIPLIER: Double = 0.6
+
+    /**
+     * The value to add against a pixel's color in order increase / decrease **brightness**.
+     *
+     * `NEW_IMG = IMAGE_MULTIPLIER * IMG + IMAGE_OFFSET`
+     */
+    @JvmField var IMAGE_OFFSET: Double = 0.0
+
+    @JvmField var BLUR_KERNEL_SIZE: Int = 2
+}
 
 class ConeScanPipeline(private val sampler: ColorSampler = defaultColorSample()) : OpenCvPipeline() {
     enum class Color {
@@ -14,21 +36,13 @@ class ConeScanPipeline(private val sampler: ColorSampler = defaultColorSample())
         private set
 
     // Matrix phases
-    private var contrasted = Mat()
+    private var filtered = Mat()
 
     override fun processFrame(input: Mat?): Mat {
-        input!!.convertTo(contrasted, -1, 0.6)
+        input!!.convertTo(filtered, -1, ConeScanConfig.IMAGE_MULTIPLIER, ConeScanConfig.IMAGE_OFFSET)
 
-        val p = contrasted.get(contrasted.rows() / 2, contrasted.cols() / 2)
+        output = sampler.sample(filtered)
 
-        output = if (p[0] >= p[1] && p[0] >= p[2]) {
-            Color.Red
-        } else if (p[1] >= p[0] && p[1] >= p[2]) {
-            Color.Green
-        } else {
-            Color.Blue
-        }
-
-        return contrasted
+        return filtered
     }
 }
